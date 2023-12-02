44fb1f191481758f59e574c150d7dd5c930031f8
CalirDeminar <edward.firespinner@gmail.com>
2023-12-02 20:32:28 +0000

    add ordering helper to graph2

--- src/graph2.rs
+++ src/graph2.rs
@@ -79,11 +79,51 @@ pub mod graph2 {
             }
             return output;
         }
+        fn get_cell_corners_in_order(&self, id: &Uuid) -> Vec<&Corner> {
+            let cell = &self.cells.get(id).unwrap();
+            let mut working_edges = cell.edges.clone();
+            let mut output_corners: Vec<Uuid> = Vec::new();
+            let mut last_edge_id = working_edges.remove(0);
+            let starting_edge = &self.edges.get(&last_edge_id).unwrap();
+            output_corners.push(starting_edge.corners.0);
+            output_corners.push(starting_edge.corners.1);
+            while working_edges.len() > 0 {
+                let next_edge_id = working_edges
+                    .iter()
+                    .find(|e_id| self.edges_share_corners(e_id, &last_edge_id))
+                    .unwrap()
+                    .clone();
+                let edge = self.edges.get(&next_edge_id).unwrap();
+                if !output_corners.contains(&edge.corners.0) {
+                    output_corners.push(edge.corners.0);
+                }
+
+                if !output_corners.contains(&edge.corners.1) {
+                    output_corners.push(edge.corners.1);
+                }
+
+                working_edges.retain(|e_id| !e_id.eq(&next_edge_id));
+                last_edge_id = next_edge_id;
+            }
+            return output_corners
+                .iter()
+                .map(|c_id| self.corners.get(c_id).unwrap())
+                .collect();
+        }
+        // edges
         fn get_edge_corners(&self, id: &Uuid) -> (&Corner, &Corner) {
             let edge = &self.edges.get(id).unwrap();
             let c_1 = self.corners.get(&edge.corners.0).unwrap();
             let c_2 = self.corners.get(&edge.corners.1).unwrap();
             return (c_1, c_2);
+        }
+        fn edges_share_corners(&self, id_1: &Uuid, id_2: &Uuid) -> bool {
+            let e_1 = self.edges.get(id_1).unwrap();
+            let e_2 = self.edges.get(id_2).unwrap();
+            return (e_1.corners.0.eq(&e_2.corners.0))
+                || (e_2.corners.0.eq(&e_2.corners.1))
+                || (e_1.corners.1.eq(&e_2.corners.0))
+                || (e_2.corners.1.eq(&e_2.corners.1));
         }
         // corners
     }
@@ -189,6 +229,83 @@ pub mod graph2 {
         #[test]
         fn gen_base_graph_test() {
             generate_base_graph(500, 100.0, 200.0);
+        }
+        #[test]
+        fn test_cell_corner_ordering() {
+            let cell_id = Uuid::new_v4();
+            let v_1_id = Uuid::new_v4();
+            let v_2_id = Uuid::new_v4();
+            let v_3_id = Uuid::new_v4();
+            let v_4_id = Uuid::new_v4();
+            let e_1_id = Uuid::new_v4();
+            let e_2_id = Uuid::new_v4();
+            let e_3_id = Uuid::new_v4();
+            let e_4_id = Uuid::new_v4();
+            let corner_1 = Corner {
+                pos: (0.0, 0.0),
+                edges: vec![e_1_id, e_4_id],
+                elevation: 0.0,
+            };
+            let corner_2 = Corner {
+                pos: (0.0, 1.0),
+                edges: vec![e_1_id, e_2_id],
+                elevation: 0.0,
+            };
+            let corner_3 = Corner {
+                pos: (1.0, 1.0),
+                edges: vec![e_2_id, e_3_id],
+                elevation: 0.0,
+            };
+            let corner_4 = Corner {
+                pos: (1.0, 0.0),
+                edges: vec![e_3_id, e_4_id],
+                elevation: 0.0,
+            };
+            let edge_1 = Edge {
+                corners: (v_1_id, v_2_id),
+                cells: vec![cell_id],
+                river: 0.0,
+            };
+            let edge_2 = Edge {
+                corners: (v_2_id, v_3_id),
+                cells: vec![cell_id],
+                river: 0.0,
+            };
+            let edge_3 = Edge {
+                corners: (v_3_id, v_4_id),
+                cells: vec![cell_id],
+                river: 0.0,
+            };
+            let edge_4 = Edge {
+                corners: (v_4_id, v_1_id),
+                cells: vec![cell_id],
+                river: 0.0,
+            };
+            let cell = Cell {
+                water: false,
+                ocean: false,
+                moisture: 0.0,
+                biome: Biome::Bare,
+                edges: vec![e_3_id, e_1_id, e_2_id, e_4_id],
+            };
+            let mut cells: HashMap<Uuid, Cell> = HashMap::new();
+            cells.insert(cell_id, cell);
+            let mut edges: HashMap<Uuid, Edge> = HashMap::new();
+            edges.insert(e_1_id, edge_1);
+            edges.insert(e_2_id, edge_2);
+            edges.insert(e_3_id, edge_3);
+            edges.insert(e_4_id, edge_4);
+            let mut corners: HashMap<Uuid, Corner> = HashMap::new();
+            corners.insert(v_1_id, corner_1);
+            corners.insert(v_2_id, corner_2);
+            corners.insert(v_3_id, corner_3);
+            corners.insert(v_4_id, corner_4);
+            let graph = Graph {
+                cells,
+                edges,
+                corners,
+            };
+            println!("{:#?}", graph.get_cell_corners_in_order(&cell_id));
         }
     }
 }
